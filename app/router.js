0000ea77c44efc5836bc13aff3b99a509c857aa8
Matt Wisner <matt@detroitweb.company>
2015-05-21 16:57:08 -0400

    really awesome login page

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('base', {path: '/'}, function(){
+
+  });
   this.route('users', function() {
     this.route('login');
     this.route('logout');
