2934cebbf401ff90e2ec5b79f25979376383cb48
Matt Wisner <matt@detroitweb.company>
2015-05-25 18:01:20 -0400

    Fixing question creation'

--- app/questions/create/route.js
+++ app/questions/create/route.js
@@ -3,18 +3,23 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
    return {
-     'title': 'asdfasdfa',
-     'content': '***',
+     'title': 'Acd',
+     'content': 'asdfasdfsadf',
+     'tags': 'zxcvzxcvzxcv'
    };
  },
 
  actions : {
     save: function() {
-      var data = this.controller.getProperties('title', 'content');
+      var data = this.controller.getProperties('title', 'content', 'tags');
       var question = this.store.createRecord('question', {
         title: data.title,
         content: data.content,
+        tags: data.tags,
       });
+      question.save();
+
     }
+
   }
 });
