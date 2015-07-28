4406e5e28c148c4d9d85b689a66c0dcead96fdad
Matt Wisner <matt@detroitweb.company>
2015-07-27 23:58:12 -0400

    Adding logo and reorganization of styles

--- app/index/route.js
+++ app/index/route.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel: function () {
+  var session = this.get('session');
+  if (!session.isAuthenticated) {
+    this.transitionTo('users.login');
+  }
+}
 });
