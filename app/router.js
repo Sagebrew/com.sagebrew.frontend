b91b3122cd866deacbf91ef78ead14dfb676066d
Matt Wisner <matt@detroitweb.company>
2015-07-27 23:08:59 -0400

    removing momemnt since it was casing js exceptions. Adding logout support. Using standard sb oauth info so that we dont have to create a new user right now. Adding some dev notes to the login page to help with user auth

--- app/router.js
+++ app/router.js
@@ -6,16 +6,19 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  'use strict';
-  this.route('solutions');
-  this.route('questions');
+  this.route('base', {path: '/'}, function(){
+    this.route("index", { path: '', resetNamespace: true });
+    this.route('solutions', { resetNamespace: true });
+    this.route('questions', { resetNamespace: true }, function() {
+      this.route('view', { path: '/question/:question_id' });
+      this.route('create');
+    });
+  });
 
   this.route('users', {path: '/users'}, function() {
     this.route('login');
     this.route('logout');
   });
-
-
 });
 
 export default Router;
