e9265c0be2540eb7d3ecc1b85253810968c37220
Matt Wisner <matt@detroitweb.company>
2015-07-29 16:53:48 -0400

    init forgot password route

--- app/router.js
+++ app/router.js
@@ -19,6 +19,7 @@ Router.map(function() {
     this.route('account', function() {
       this.route('login');
       this.route('logout');
+      this.route('forgot');
     });
   });
 });
---
+++ tests/unit/users/account/forgot/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:users/account/forgot', 'Unit | Route | users/account/forgot', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
