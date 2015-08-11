f3b64bfebdbd5b0a5750d188018e7dfd70e04140
Matt Wisner <matt@detroitweb.company>
2015-08-11 17:26:41 -0400

    Some risky styles for questions area

--- app/question/model.js
+++ app/question/model.js
@@ -2,6 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr(),
+  created: DS.attr('date'),
   content: DS.attr(),
-  tags: DS.attr()
+  tags: DS.attr(),
+  profile: DS.belongsTo('profile'),
+  solutions: DS.hasMany({ async: true }, 'solutions')
 });
--- app/solution/model.js
+++ app/solution/model.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  created: DS.attr('date'),
+  content: DS.attr(),
+  profile: DS.belongsTo('profile')
 });
