0b277fc68354a17bfa7d2d612fc97121bcf1efac
Matt Wisner <matt@detroitweb.company>
2015-08-12 22:11:20 -0400

    Colors on the votes

--- app/components/posts/post-summary/component.js
+++ app/components/posts/post-summary/component.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 import config from '../../../config/environment';
 
 export default Ember.Component.extend({
+  isUrgent : Ember.computed(function() {
+    return "active";
+  }),
   actions : {
     upvote() {
-      console.log("upvote");
-      console.log(this.get('post'));
-      console.log(config);
       var url = config.APP.API_HOST + "/" + config.APP.API_NAMESPACE;
       return Ember.$.ajax({
         url:  url + "/" + "posts/" + this.get('post').id + "/votes/?expedite=true",
