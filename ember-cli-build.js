d093c7da1418a9c2677869b4f4c31461035816ef
Matt Wisner <matt@detroitweb.company>
2015-08-15 12:04:24 -0400

    Fix a few deprecations

---
+++ ember-cli-build.js
@@ -0,0 +1,11 @@
+/* global require, module */
+
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+    var app = new EmberApp(defaults, {
+        // Any other options
+    });
+
+    return app.toTree();
+};
