65c7bc1286488cc0b3d125daa4078af79b126e30
Devon Bleibtrey <bleib1dj@gmail.com>
2015-08-15 16:31:42 -0400

    Fixing deployment tags

--- deployment/master.sh
+++ deployment/master.sh
@@ -7,6 +7,10 @@ EB_BUCKET=massive-dangerzone-$CIRCLE_BRANCH
 DOCKERRUN_FILE=$SHA1-master.json
 DOCKERRUN_FILE_SYS=$SHA1-master_mass-sys.aws.json
 
+sed "s/<TAG>/$SHA1/" < ~/com.sagebrew.frontend/deployment/Dockerrun.aws.sys.master.json.template > $DOCKERRUN_FILE_SYS
+sed "s/<TAG>/$SHA1/" < ~/com.sagebrew.frontend/deployment/Dockerrun.aws.master.json.template > $DOCKERRUN_FILE
+
+
 aws s3 cp ~/com.sagebrew.frontend/deployment/Dockerrun.aws.sys.master.json s3://$BUCKET_STAGING/docker_config/$DOCKERRUN_FILE_SYS
 aws s3 cp ~/com.sagebrew.frontend/deployment/Dockerrun.aws.master.json s3://$BUCKET_STAGING/docker_config/$DOCKERRUN_FILE
 
--- deployment/staging.sh
+++ deployment/staging.sh
@@ -7,8 +7,11 @@ EB_BUCKET=massive-dangerzone-$CIRCLE_BRANCH
 DOCKERRUN_FILE=$SHA1-staging.json
 DOCKERRUN_FILE_SYS=$SHA1-staging_mass-sys.aws.json
 
-aws s3 cp ~/com.sagebrew.frontend/deployment/Dockerrun.aws.sys.staging.json s3://$BUCKET_STAGING/docker_config/$DOCKERRUN_FILE_SYS
-aws s3 cp ~/com.sagebrew.frontend/deployment/Dockerrun.aws.staging.json s3://$BUCKET_STAGING/docker_config/$DOCKERRUN_FILE
+sed "s/<TAG>/$SHA1/" < ~/com.sagebrew.frontend/deployment/Dockerrun.aws.sys.staging.json.template > $DOCKERRUN_FILE_SYS
+sed "s/<TAG>/$SHA1/" < ~/com.sagebrew.frontend/deployment/Dockerrun.aws.staging.json.template > $DOCKERRUN_FILE
+
+aws s3 cp ~/com.sagebrew.frontend/deployment/$DOCKERRUN_FILE_SYS s3://$BUCKET_STAGING/docker_config/$DOCKERRUN_FILE_SYS
+aws s3 cp ~/com.sagebrew.frontend/deployment/$DOCKERRUN_FILE s3://$BUCKET_STAGING/docker_config/$DOCKERRUN_FILE
 
 ~/virtualenvs/venv-2.7.6/bin/python ~/com.sagebrew.frontend/deployment/populate_options.py
 ~/virtualenvs/venv-2.7.6/bin/python ~/com.sagebrew.frontend/deployment/deployment.py staging
