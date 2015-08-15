16f30c59c6f094e010859cc36b5ba9eec9739b64
Devon Bleibtrey <bleib1dj@gmail.com>
2015-08-15 13:53:29 -0400

    Moving deployment to sys util since aws doesn't allow outside access to redis easily

--- deployment/master.sh
+++ deployment/master.sh
@@ -2,7 +2,13 @@
 
 SHA1=$1
 docker push sagebrew/sb_app:$SHA1
+docker push sagebrew/mass_sys:$SHA1
 EB_BUCKET=massive-dangerzone-$CIRCLE_BRANCH
 DOCKERRUN_FILE=Dockerrun.aws.master.json
+DOCKERRUN_FILE_SYS=$SHA1-master_mass-sys.aws.json
 
+aws s3 cp ~/com.sagebrew.fontend/deployment/Dockerrun.aws.json.sys.master s3://$EB_BUCKET/docker_config/$DOCKERRUN_FILE_SYS
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/docker_config/Dockerrun.aws.json
+
+
+~/virtualenvs/venv-system/bin/python ~/com.sagebrew.frontend/deployment/deployment.py master
--- deployment/staging.sh
+++ deployment/staging.sh
@@ -2,7 +2,13 @@
 
 SHA1=$1
 docker push sagebrew/sb_app:$SHA1
+docker push sagebrew/mass_sys:$SHA1
 EB_BUCKET=massive-dangerzone-$CIRCLE_BRANCH
 DOCKERRUN_FILE=Dockerrun.aws.staging.json
+DOCKERRUN_FILE_SYS=$SHA1-staging_mass-sys.aws.json
 
+aws s3 cp ~/com.sagebrew.fontend/deployment/Dockerrun.aws.json.sys.staging s3://$EB_BUCKET/docker_config/$DOCKERRUN_FILE_SYS
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/docker_config/Dockerrun.aws.json
+
+
+~/virtualenvs/venv-system/bin/python ~/com.sagebrew.frontend/deployment/deployment.py staging
