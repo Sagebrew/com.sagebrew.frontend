cf8ed9ac2570fc59d94935fd2a938f6708bc5c7f
Devon Bleibtrey <bleib1dj@gmail.com>
2015-08-16 16:51:51 -0400

    Working on cloudfront deployment

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,8 +3,30 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
+    var env = EmberApp.env();
+    var isProductionLikeBuild = ['production', 'staging'].indexOf(env) > -1;
     var app = new EmberApp(defaults, {
-        // Any other options
+      fingerprint: {
+        enabled: isProductionLikeBuild,
+        prepend: 'https://d6tmbwuijbg7w.cloudfront.net/'
+      },
+      sourcemaps: {
+        enabled: !isProductionLikeBuild
+      },
+      minifyCSS: { enabled: isProductionLikeBuild },
+      minifyJS: { enabled: isProductionLikeBuild },
+
+      tests: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild,
+      hinting: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild,
+
+      vendorFiles: {
+        'handlebars.js': {
+          staging:  'bower_components/handlebars/handlebars.runtime.js'
+        },
+        'ember.js': {
+          staging:  'bower_components/ember/ember.prod.js'
+        }
+      }
     });
 
     return app.toTree();
