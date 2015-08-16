9274528bc96aa13eb8141327d387ee33f7fd9c35
Devon Bleibtrey <bleib1dj@gmail.com>
2015-08-16 19:53:48 -0400

    Trying to fix pull request issue

---
+++ dockerfiles/populate_docker.py
@@ -0,0 +1,28 @@
+from os import environ, path
+
+
+def populate_config():
+    project_dir = path.dirname(path.dirname(path.realpath(__file__)))
+    with open("%s/dockerfiles/dockerfile.template" % (
+            project_dir), "r") as dockerfile:
+        data = dockerfile.read()
+        data = populate_values(data)
+
+    f = open("%s/dockerfiles/Dockerfile" % (project_dir), "w")
+    f.write(data)
+    f.close()
+
+
+def populate_values(data):
+    environment_set = environ.get("CIRCLE_BRANCH", "")
+    if environment_set != "staging" and environment_set != "master":
+        environment_set = "development"
+    data = data.replace("<ENVIRONMENT>", environment_set)
+    data = data.replace("<BRANCH>", environ.get('CIRCLE_BRANCH', ""))
+    data = data.replace("<APP_USER>", environ.get("APP_USER", ""))
+    data = data.replace("<BUCKET>", environ.get("BUCKET_STAGING", ""))
+    return data
+
+
+if __name__ == "__main__":
+    populate_config()
